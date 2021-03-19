e64b72a0183e1f2a293d8be81930ec55205b6aa9
bewisesh91 <bewise.seunghyun@gmail.com>
2021-03-19 23:16:43 +0900

    python

---
+++ practice/HW4_2010_10057_SeunghyunMun/P1.py
@@ -0,0 +1,26 @@
+"""
+Practical programming Chapter 8 Exercise 7  
+
+**Instruction**
+Precondition: len(L) >= 2
+Return True if and only if first item of the list is the same as the last.
+>>> P1([3, 4, 2, 8, 3])
+True
+>>> P1(['apple', 'banana', 'pear'])
+False
+>>> P1([4.0, 4.5])
+False
+
+Complete P1 function
+"""
+
+def P1(L:list) -> bool:
+    ##### Write your Code Here #####
+    if L[0] == L[-1] :
+        return True
+    else :
+        return False
+    ##### End of your code #####
+
+
+
