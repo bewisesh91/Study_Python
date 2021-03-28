f28795b90e40460ba2c7a80dda91962e26befdf2
bewisesh91 <bewisesh91@likelion.org>
2021-03-28 14:02:23 +0900

    practice

--- practice/HW6_2010_10057_SeunghyunMun/P1.py
+++ practice/HW6_2010_10057_SeunghyunMun/P1.py
@@ -1,11 +1,14 @@
 class Country:
     def __init__(self, name, population, area):
+        self.name = name
+        self.population = population
+        self.area = area
 
-    
     def is_larger(self, other):
-
+        if self.area > other.area :
+            return True
+        else :
+            return False
     
-    def population_density(self):
-
-
-    
+    def population_density(self): 
+        return self.population / self.area
--- practice/HW6_2010_10057_SeunghyunMun/P2.py
+++ practice/HW6_2010_10057_SeunghyunMun/P2.py
@@ -2,6 +2,11 @@ from P1 import Country # Do Not Modify
 
 class Continent:
     def __init__(self, name, countries):
-
+        self.name = name
+        self.countries = countries
         
     def total_population(self):
+        total_population = 0
+        for i in self.countries :
+            total_population += i.population
+        return total_population
